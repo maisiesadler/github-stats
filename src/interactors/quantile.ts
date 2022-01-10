7b16c026c750d0f01fee541320f77f922f2dfcd6
Maisie <maisiesadler@hotmail.com>
2022-01-10 19:20:13 +0000

    include friendly time

--- src/interactors/quantile.ts
+++ src/interactors/quantile.ts
@@ -1,11 +1,13 @@
-export const quantile = (sortedArray: number[], percentile: number) => {
-    if (percentile <= 0 || percentile > 100) {
-        return { ok: false }
-    }
+export const quantile
+    : (sortedArray: number[], percentile: number) => { ok: boolean, value?: number }
+    = (sortedArray: number[], percentile: number) => {
+        if (percentile <= 0 || percentile > 100) {
+            return { ok: false }
+        }
 
-    const kIndex = Math.ceil(sortedArray.length * (percentile / 100)) - 1;
-    return {
-        ok: true,
-        value: sortedArray[kIndex],
+        const kIndex = Math.ceil(sortedArray.length * (percentile / 100)) - 1;
+        return {
+            ok: true,
+            value: sortedArray[kIndex],
+        }
     }
-}
--- src/interactors/summarize-time-to-close.ts
+++ src/interactors/summarize-time-to-close.ts
@@ -1,13 +1,15 @@
 import { quantile } from ".";
+import { GetReadableSeconds } from "./readable-seconds"
 import { GetTimeToClose, PR } from "./time-to-close"
 
 export class SummarizeTimeToClose {
     public Execute(percentiles: number[], prs: PR[])
-        : { ok: boolean, value?: number, p: number }[] {
+        : { ok: boolean, value?: number, p: number, friendly?: string }[] {
 
         let seconds: number[] = []
 
         const getTimeToClose = new GetTimeToClose();
+        const getReadableSeconds = new GetReadableSeconds();
         prs.forEach(pr => {
             const result = getTimeToClose.Execute(pr)
             // console.log(result)
@@ -17,6 +19,14 @@ export class SummarizeTimeToClose {
         // sort array ascending
         seconds = seconds.sort((a, b) => a - b);
 
-        return percentiles.map(p => ({ p, ...quantile(seconds, p) }))
+        return percentiles.map(p => {
+            const result: { ok: boolean, value?: number, p: number, friendly?: string }
+                = { p, ...quantile(seconds, p) }
+
+            if (result.ok) {
+                result.friendly = getReadableSeconds.Execute(result.value).text
+            }
+            return result
+        })
     }
 }
