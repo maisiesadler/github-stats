665b735c8ce75e439d305fc0b2b8823266cfeb4a
Maisie <maisiesadler@hotmail.com>
2022-01-10 19:13:22 +0000

    add hours

--- src/interactors/readable-seconds.ts
+++ src/interactors/readable-seconds.ts
@@ -1,16 +1,29 @@
+const secondsInHour = 60 * 60
+const secondsInMinute = 60
+
 export class GetReadableSeconds {
     public Execute(totalSeconds: number): { text: string } {
         if (totalSeconds < 0) return { text: 'Invalid' }
 
-        const minutes = Math.floor(totalSeconds / 60)
-        const seconds = totalSeconds - minutes * 60
+        let remaining = totalSeconds
 
-        const text = this.asText({ minutes, seconds })
+        const hours = Math.floor(totalSeconds / secondsInHour)
+        remaining = remaining - hours * secondsInHour
+        const minutes = Math.floor(remaining / secondsInMinute)
+        const seconds = remaining - minutes * secondsInMinute
+
+        const text = this.asText({ hours, minutes, seconds })
         return { text }
     }
 
-    private asText({ minutes, seconds }: { minutes: number, seconds: number }) {
+    private asText({ hours, minutes, seconds }: { hours: number, minutes: number, seconds: number }) {
         const parts = []
+
+        if (hours === 1) {
+            parts.push('1 hour')
+        } else if (hours > 0) {
+            parts.push(`${hours} hours`)
+        }
 
         if (minutes === 1) {
             parts.push('1 minute')
--- test/interactors/readable-seconds.ts
+++ test/interactors/readable-seconds.ts
@@ -2,6 +2,8 @@ import { expect } from 'chai';
 import 'mocha';
 import { GetReadableSeconds } from '../../src/interactors';
 
+const oneHour = 60 * 60
+
 describe('readable seconds', function () {
 
     [
@@ -23,14 +25,17 @@ describe('readable seconds', function () {
         { seconds: 60, expectedText: '1 minute' },
         { seconds: 600, expectedText: '10 minutes' },
         { seconds: 606, expectedText: '10 minutes and 6 seconds' },
+        { seconds: oneHour + 606, expectedText: '1 hour and 10 minutes and 6 seconds' },
+        { seconds: oneHour + 6, expectedText: '1 hour and 6 seconds' },
+        { seconds: 6 * oneHour + 6, expectedText: '6 hours and 6 seconds' },
     ].forEach(({ seconds, expectedText }) => {
-            it(`Seconds and minutes: ${seconds}`, async () => {
+        it(`Seconds and minutes: ${seconds}`, async () => {
 
-                const interactor = new GetReadableSeconds()
+            const interactor = new GetReadableSeconds()
 
-                const result = interactor.Execute(seconds)
+            const result = interactor.Execute(seconds)
 
-                expect(result.text).to.equal(expectedText)
-            })
-        });
+            expect(result.text).to.equal(expectedText)
+        })
+    });
 })
